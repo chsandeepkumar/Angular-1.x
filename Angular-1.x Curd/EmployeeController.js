153d5fde7600845c68ac4317ec76de0405bc436f
chintalapally sandeep reddy <ch.sandeepreddy544@gmail.com>
2017-10-04 22:38:00 -0500

    Updated File with proper response data

--- Angular-1.x Curd/EmployeeController.js
+++ Angular-1.x Curd/EmployeeController.js
@@ -1,12 +1,12 @@
-//creating anangular application module
+//creating anangular application module sandeep
 var employeeAppModule = angular.module("employeeApp", []);
 
 //The below code will read the data from EmployeeData.json file and will assign the data to the $scope variable 
 employeeAppModule.controller("employeeController", function($scope, $http){    
-            $http.get('student.json') //$http.get method is used to reading the EmployeeData.json file
+            $http.get('Employeedata.json') //$http.get method is used to reading the EmployeeData.json file
             
-                .then (function(data){
-                        $scope.employees = data; // binding the data to the $scope variable
+                .then (function(response){
+                        $scope.employees = response.data; // binding the data to the $scope variable
                     })
                 ,(function(error) {
                         console.error('Unexpected error occured while loading the employee data', status, data);
